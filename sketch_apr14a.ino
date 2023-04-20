77441668d173372704b92d30fc6a3fd2d147980c
JSBA2022235 <128553420+JSBA2022235@users.noreply.github.com>
2023-04-20 11:27:51 -0600

    Add files via upload

---
+++ sketch_apr14a.ino
@@ -0,0 +1,103 @@
+/*
+Fundacion Kinal
+Centro Educativo Tecnico Laboral
+Electronica
+Grado: Quinto
+Seccion: A
+Cuerso: Taller de Electronica Digital y Reparacion de
+Computadoras I
+Nombre: Jose Sebastian Bocaletti Aguilar
+Carnet: 2022235
+*/
+#define red1 2
+#define red2 3
+#define red3 4
+#define red4 5
+#define red5 6
+#define red6 7
+#define red7 8
+#define red8 9
+#define red9 10
+#define red10 11
+
+void setup() {
+   
+  
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red1,HIGH);
+  delay(500);
+  digitalWrite(red1,LOW);
+  digitalWrite(red2,HIGH);
+  delay(500);
+  digitalWrite(red2,LOW);
+  digitalWrite(red3,HIGH);
+  delay(500);
+  digitalWrite(red3,LOW);
+  digitalWrite(red4,HIGH);
+  delay(500);
+  digitalWrite(red4,LOW);
+  digitalWrite(red5,HIGH);
+  delay(500);
+  digitalWrite(red5,LOW);
+  digitalWrite(red6,HIGH);
+  delay(500);
+  digitalWrite(red6,LOW);
+  digitalWrite(red7,HIGH);
+  delay(500);
+  digitalWrite(red7,LOW);
+  digitalWrite(red8,HIGH);
+  delay(500);
+  digitalWrite(red8,LOW);
+  digitalWrite(red9,HIGH);
+  delay(500);
+  digitalWrite(red9,LOW);
+  digitalWrite(red10,HIGH);
+  delay(500);
+  digitalWrite(red10,LOW);
+  
+ 
+  digitalWrite(red10,HIGH);
+  delay(500);
+  digitalWrite(red10,LOW);
+  digitalWrite(red9,HIGH);
+  delay(500);
+  digitalWrite(red9,LOW);
+  digitalWrite(red8,HIGH);
+  delay(500);
+  digitalWrite(red8,LOW);
+  digitalWrite(red7,HIGH);
+  delay(500);
+  digitalWrite(red7,LOW);
+  digitalWrite(red6,HIGH);
+  delay(500);
+  digitalWrite(red6,LOW);
+  digitalWrite(red5,HIGH);
+  delay(500);
+  digitalWrite(red5,LOW);
+  digitalWrite(red4,HIGH);
+  delay(500);
+  digitalWrite(red4,LOW);
+  digitalWrite(red3,HIGH);
+  delay(500);
+  digitalWrite(red3,LOW);
+  digitalWrite(red2,HIGH);
+  delay(500);
+  digitalWrite(red2,LOW);
+  digitalWrite(red1,HIGH);
+  delay(500);
+  digitalWrite(red1,LOW);
+  
+ 
+}
